2e2783f0bc439a6c9c2f3bf5e29170b19a9be2cc
YuriyGubin <iosdevelop.2022@gmail.com>
2022-10-04 22:59:40 +0500

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Yuriy on 04.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
