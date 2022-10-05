e5d9a65423e6683f98229fbe9eb7e69b4ca3542a
YuriyGubin <iosdevelop.2022@gmail.com>
2022-10-05 13:19:13 +0500

    Add WelcomeViewController

--- AboutMe/LoginViewController.swift
+++ AboutMe/LoginViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBOutlet var usernameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    private let username = "123"
+    private let password = "123"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
         
     }
 
+    @IBAction func loginButtonPressed() {
+        guard usernameTF.text == username, passwordTF.text == password else {
+            showAlert(
+                title: "Incorrect Login or Password 😎",
+                message: "Please, enter correct login and password",
+                textField: passwordTF
+            )
+            return
+        }
+    }
+    
+    @IBAction func forgotButtonsPressed(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(title: "Hey there 👋", message: "Your name: \(username)")
+        : showAlert(title: "Hey there 👋", message: "Your password \(password)")
+    }
+}
 
+extension LoginViewController {
+    func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
---
+++ AboutMe/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  AboutMe
+//
+//  Created by Yuriy on 05.10.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
