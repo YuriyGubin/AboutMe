093e23e6ba54524bfc236317a9c91904400d96a2
YuriyGubin <iosdevelop.2022@gmail.com>
2022-10-07 11:57:58 +0500

    HomeWork

--- AboutMe/Models/User.swift
+++ AboutMe/Models/User.swift
@@ -6,8 +6,63 @@
 //
 
 struct User {
-    let username = "Yuriy Gubin"
-    let password = "222"
-    let person = Person.getPerson()
+    let login: String
+    let password: String
+    let person: Person
+    
+    static func getUser() -> User {
+        User(
+            login: "iosdev",
+            password: "123",
+            person: Person.getPerson()
+        )
+    }
+}
+
+struct Person {
+    
+    let name: String
+    let surname: String
+    let photo: String
+    let job: Company
+    let bio: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getPerson() -> Person {
+        Person(
+            name: "Юрий",
+            surname: "Губин",
+            photo: "Unknown",
+            job: Company.getCompany(),
+            bio: "Истинный айосер. Характер — нордический, выдержанный. С товарищами по чату поддерживает хорошие отношения. Безукоризненно выполняет свой ученический долг. Беспощаден к ошибкам в коде. Отличный спортсмен: бег, плавание, большой теннис. Холост. В связях, порочащих его, замечен не был. Наградами в сфере разработки приложений пока не отмечен."
+        )
+    }
+}
+
+struct Company {
+    let title: String
+    let jobTitle: JobTitle
+    let department: Department
+    
+    static func getCompany() -> Company {
+        Company(
+        title: "птицефабрика Боровская",
+        jobTitle: .specialist,
+        department: .itDepartment
+        )
+    }
+}
+
+enum JobTitle: String {
+    case specialist = "IT специалист"
+    case head = "IT Head"
+}
+
+enum Department: String {
+    case itDepartment = "IT отдел"
+    case management = "Менеджмент"
 }
 
